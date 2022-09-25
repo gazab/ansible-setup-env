d13007580c0658e49208ff219cc6520db19ea942
Gustav Tonér <gustav.toner@sr.se>
2022-09-25 20:45:35 +0200

    Refactor and add bitwarden cli installation

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
-
-if [ -z "$DISPLAY" ]
+if [ "$1" ]
+then
+      echo "Running install playbook with tags: $1"
+      ansible-playbook install.yml --ask-become-pass --tags "$1"
+elif [ -z "$DISPLAY" ]
 then
       echo "GUI not found. Only installing CLI tools"
       ansible-playbook install.yml --ask-become-pass --tags "cli"
 else
       echo "GUI found. Installing everything"
-      ansible-playbook install.yml --ask-become-pass
+      ansible-playbook install.yml --ask-become-pass --tags
 fi
